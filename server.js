707e069b07012227a63da7768159fac2a3ac4cf5
Doan Le Thieu <thieurom@gmail.com>
2016-11-28 18:03:40 +0700

    Get app working

--- server.js
+++ server.js
@@ -5,7 +5,23 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.send('It works!');
+    let ipaddress = '',
+        language = '',
+        software = '';
+
+    let userAgent;
+    
+    ipaddress = req.headers['x-forwared-for'];
+    language = req.headers['accept-language'].split(';')[0].split(',')[0];
+
+    userAgent = req.headers['user-agent'];
+    software = userAgent.substring(userAgent.indexOf('(') + 1, userAgent.indexOf(')'));
+
+    res.json({
+        ipaddress,
+        language,
+        software
+    });
 });
 
 app.listen(port, () => {
